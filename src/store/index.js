f95dd51d159453ccd6d63668bf604bb6fcdf3bad
c2498748521 <72338566+c2498748521@users.noreply.github.com>
2020-12-12 10:56:48 +0800

    8-2

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    collapsed: false, // 是否隐藏左部导航，false为不隐藏，true为隐藏
   },
   mutations: {
+    toggleCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+
   },
   modules: {
   },
